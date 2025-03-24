68d987a7219fc8d1158a49bb30d395eb986b4834
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2025-03-24 17:05:28 +0200

    Revert "Fix result timeline entry description formatting"
    This reverts commit 27e66067b109eeb7e4c188d03920dde430c8d430.
    
    See:
    https://github.com/decidim/decidim/pull/14171
    
    Fixed in version 0.27.10.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
 require "decidim/core/test/factories"
-require "decidim/accountability/test/factories"
 require "decidim/budgets/test/factories"
 require "decidim/proposals/test/factories"
